04d856ad04428065c7aa69808accce5b0000a3d1
sai051192 <61408815+sai051192@users.noreply.github.com>
2020-12-06 16:16:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var time = require('time');
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date(); 
-    currentTime.setTimezone("America/Los_Angeles");
+    currentTime.setTimezone("Asia/Kolkata");
     callback(null, {
         statusCode: '200',
-        body: 'Dude, The time in Los Angeles is: ' + currentTime.toString(),
+        body: 'The time in India is: ' + currentTime.toString(),
     });
 };
